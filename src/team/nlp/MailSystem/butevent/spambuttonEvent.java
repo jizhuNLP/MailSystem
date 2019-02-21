675989a5e006d42bc0f8f92845114acdfa09709a
Cian chen <Cianc_@outlook.com>
2019-02-21 21:03:55 +0800

    添加了垃圾邮件功能按钮实现

---
+++ src/team/nlp/MailSystem/butevent/spambuttonEvent.java
@@ -0,0 +1,14 @@
+package team.nlp.MailSystem.butevent;
+
+/**
+* @author Cianc
+* @version 创建时间：2019年2月21日 下午8:51:58
+* @ClassName spambuttonEvent
+* @Description 该类为垃圾邮件按钮得功能实现，具体实现功能为：
+* 				
+*/
+public class spambuttonEvent {
+	spambuttonEvent(){
+		
+	}
+}
